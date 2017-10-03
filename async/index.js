5bc8e5a5b9805f1677d336961a66b852bc1a7f3f
Jon Thompson <jt@zoomer.io>
2017-10-03 13:59:00 -0400

    Implement parallel async function

--- async/index.js
+++ async/index.js
@@ -13,4 +13,24 @@ module.exports = {
       processThunk(0);
     };
   },
+
+  parallel(thunks) {
+    return function (finalCallback) {
+      const data = Array(thunks.length);
+      let completedThunks = 0;
+
+      function processThunk(thunk, index) {
+        thunk((error, thunkData) => {
+          data[index] = thunkData;
+          completedThunks++;
+
+          if (completedThunks === thunks.length) {
+            finalCallback(null, data);
+          }
+        });
+      }
+
+      thunks.forEach(processThunk);
+    };
+  },
 };
